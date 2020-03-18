1d7739db525159aca3ad7a1116e0137634445e3d
TyOzzy <tjo32@byu.edu>
2020-03-18 16:11:42 -0600

    added shopping cart logo

--- src/mock-data.js
+++ src/mock-data.js
@@ -2,30 +2,35 @@ let mock = [{
     id: 1,
     name: "Collagen Powder",
     price: "$49.99",
+	type: "supplement",
     image: 'collagenpowder.jpg'
   },
   {
     id: 2,
     name: "Multivitamin",
     price: "$29.99",
+	type: "vitamin",
     image: "multivitamin.jpg"
   },
   {
     id: 3,
     name: "Fat Burner",
     price: "$19.99",
+	type: "supplement",
     image: 'fatburner.png'
   },
   {
     id: 4,
     name: "MCT Oil",
     price: "$14.99",
+	type: "supplement",
     image: 'mctoil.jpg'
   },
   {
     id: 5,
     name: "Prenatal Supplement",
     price: "$9.99",
+	type: "supplement",
     image: 'prenatal.jpg'
 
   },
@@ -33,6 +38,7 @@ let mock = [{
     id: 6,
     name: "Antioxidant and Fiber Blend",
     price: "$19.99",
+	type: "supplement",
     image: 'antioxidantfruitandfiber.jpg'
   }
 ]
