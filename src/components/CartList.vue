88df14a50b102862ec2d2161ecd8426ed4e7516e
Kinsley Merrill <kinsleychristensen@Kinsleys-MacBook-Pro.local>
2020-03-18 11:37:00 -0600

    new commit

---
+++ src/components/CartList.vue
@@ -0,0 +1,105 @@
+<template>
+<div class="wrapper">
+  <div class="cart">
+    <div class="product" v-for="product in cart" :key="product.id">
+      <div class="info">
+        <h1>{{product.name}}</h1>
+      </div>
+      <div class="image">
+        <img :src="'/images/products/'+product.image">
+      </div>
+      <div class="price">
+        <h2>{{product.price}}</h2>
+        <button v-on:click="removeFromCart(product)" class="auto">Remove</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CartList',
+  props: {
+    cart: Array
+  },
+  methods: {
+    removeFromCart(product) {
+      var index = this.$root.$data.cart.indexOf(product);
+      if (index > -1)
+      this.$root.$data.cart.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.cart {
+  margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.product {
+  margin: 20px;
+  margin-top: 25px;
+  width: 150px;
+}
+
+.product img {
+  border: 2px solid #333;
+  height: 190px;
+  width: 150px;
+  object-fit: cover;
+}
+
+.product .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background: #B33C00;
+  color: #000;
+  padding: 5px 15px;
+  height: 60px;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+
+.price {
+  display: flex;
+}
+
+button {
+  font-size: 12px;
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <h1>Cart</h1>
+    </div>
+    <div class="empty">
+      <p v-show="this.$root.$data.cart.length === 0">The cart is currently empty.</p>
+    </div>
+    <CartList :cart="cart" />
+  </div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.empty {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
